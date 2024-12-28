d5016652fd699fadef9e9cccc3a81e017f27dec7
unknown <polibos6366@gmail.com>
2024-12-27 19:34:25 -0700

    global error handler created

--- server/index.js
+++ server/index.js
@@ -9,6 +9,13 @@ const PORT = 8000;
 app.use("/api/user", authRoutes);
 app.use("/api/tasks", taskRoutes);
 
+//global error handler
+app.use((err, req, res, next) => {
+  const statusCode = err.statusCode || 500;
+  const message = err.message || "Internal server error";
+  res.status(statusCode).json({ error: message });
+});
+
 app.listen(PORT, () => {
   console.log(`Listening on port ${PORT}`);
 });
