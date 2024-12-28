f83d75eecbfeb8749c9d41c23ff6f9d27d52802c
unknown <polibos6366@gmail.com>
2024-12-27 18:05:17 -0700

    Authentication for login and register routes created

--- server/index.js
+++ server/index.js
@@ -1,7 +1,11 @@
 import express from "express";
+import authRoutes from "./routes/auth.js";
 
 const app = express();
 const PORT = 8000;
+
+// middlewares
+app.use("/api/user", authRoutes);
 
 app.listen(PORT, () => {
   console.log(`Listening on port ${PORT}`);
