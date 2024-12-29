f3e83c22ff289c7f839037b6ab03ce229b5e666a
unknown <polibos6366@gmail.com>
2024-12-29 10:35:48 -0700

    Server structure created, and initiated, task model and task controllers created

---
+++ server/controllers/taskControllers.js
@@ -0,0 +1,78 @@
+import Task from "../models/task.js";
+import { validationResult } from "express-validator";
+
+export const createTask = async (req, res) => {
+  const errors = validationResult(req);
+
+  if (!errors.isEmpty()) {
+    return res.status(400).json({ errors: errors });
+  }
+
+  try {
+    const { title, description } = req.body;
+    const task = new Task({ title, description });
+
+    await task.save();
+    res.status(201).json(task);
+  } catch (error) {
+    res.status(500).json({ message: "Server error", error: error.message });
+  }
+};
+
+export const getTasks = async (req, res) => {
+  try {
+    const { completed } = req.query;
+
+    const filter = completed ? { completed: completed === "true" } : {};
+
+    const tasks = await Task.find(filter);
+
+    res.status(200).json(tasks);
+  } catch (error) {
+    res.status(500).json({ message: "Server error", error: error.message });
+  }
+};
+
+export const getTaks = async (req, res) => {
+  try {
+    const task = await Task.findById(req.params.id);
+
+    if (!task) {
+      return res.status(404).json({ message: "Task not found" });
+    }
+
+    res.status(200).json(task);
+  } catch (error) {
+    res.status(500).json({ message: "Server error", error: error.message });
+  }
+};
+
+export const updateTask = async (req, res) => {
+  try {
+    const task = await Task.findByIdAndUpdate(req.params.id, req.body, {
+      new: true,
+    });
+
+    if (!task) {
+      return res.status(404).json({ message: "Task not found" });
+    }
+
+    res.status(200).json(task);
+  } catch (error) {
+    res.status(500).json({ message: "Server error", error: error.message });
+  }
+};
+
+export const deleteTask = async (req, res) => {
+  try {
+    const task = await Task.findByIdAndDelete(req.params.id);
+
+    if (!task) {
+      return res.status(404).json({ message: "Task not found" });
+    }
+
+    res.status(200).json({ message: "Task deleted" });
+  } catch (error) {
+    res.status(500).json({ message: "Server error", error: error.message });
+  }
+};
---
+++ server/index.js
@@ -0,0 +1,41 @@
+import express from "express";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+import cors from "cors";
+
+// import taskRoutes from "./routes/taskRoutes.js";
+// import { swaggerDocs } from "./docs/swagger.js";
+// import errorHandler from "./middleware/errorMiddleware.js";
+
+dotenv.config();
+
+const app = express();
+const PORT = process.env.PORT || 5000;
+
+// Middlewares
+app.use(express.json());
+app.use(cors());
+// app.use("/api/tasks", taskRoutes);
+// app.use(errorHandler);
+
+//Swagger documentation
+// swaggerDocs(app, PORT);
+
+//MongoDB connection
+
+const connectDB = async () => {
+  try {
+    await mongoose.connect(process.env.MONGO_URI);
+
+    console.log("MongoDB connected");
+  } catch (error) {
+    console.error("Error connecting to MongoDB:", error.message);
+    process.exit(1);
+  }
+};
+
+connectDB();
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
